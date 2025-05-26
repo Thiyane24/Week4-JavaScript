4693123065b5e0b6999c277379dcc746e8d962f3
Thiyane24 <thiyanexavier6@gmail.com>
2025-05-26 22:08:02 +0200

    lesson 1, 2

--- script.js
+++ script.js
@@ -3,3 +3,15 @@
                 document.getElementById("text1").innerHTML="How are you?";
 
         }
+
+        function lesson2() {
+            var message1 = "Hello everyone!";
+            var message2 ="I love JavaScript!";
+            var message3 = "It's awesome!";
+
+            document.getElementById("heading2").innerHTML= message1;
+            document.getElementById("text2").innerHTML= message2 + " "+ message3;
+        
+
+            
+        }
